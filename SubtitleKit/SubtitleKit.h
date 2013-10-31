1741ad7d047704d7a6a18b0532194a56520f552e
Maxthon Chan <xcvista@me.com>
2013-10-31 14:57:51 +0800

    Initial Commit

---
+++ SubtitleKit/SubtitleKit.h
@@ -0,0 +1,13 @@
+//
+//  SubtitleKit.h
+//  SubtitleKit
+//
+//  Created by Maxthon Chan on 10/31/13.
+//  Copyright (c) 2013 muski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubtitleKit : NSObject
+
+@end
---
+++ SubtitleKit/SubtitleKit.m
@@ -0,0 +1,13 @@
+//
+//  SubtitleKit.m
+//  SubtitleKit
+//
+//  Created by Maxthon Chan on 10/31/13.
+//  Copyright (c) 2013 muski. All rights reserved.
+//
+
+#import "SubtitleKit.h"
+
+@implementation SubtitleKit
+
+@end
